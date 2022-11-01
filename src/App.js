7af935645b3407662e8f41856c3cfc3b79fd5ef1
João Victor <joao.lenny100@gmail.com>
2022-10-31 21:45:24 -0300

    App
    - Importando "style.css";
    - Adicionando className "page".

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+
+import './css/style.css';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSystem';
@@ -6,7 +8,7 @@ import SolarSystem from './components/SolarSystem';
 class App extends React.Component {
   render() {
     return (
-      <div>
+      <div className="page">
         <Header />
         <SolarSystem />
         <Missions />
