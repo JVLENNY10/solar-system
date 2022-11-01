9e5d13b161c347d9af6b5f9d6108b0c8204f8157
João Victor <joao.lenny100@gmail.com>
2022-10-31 21:42:38 -0300

    Components - Missions
    - Adicionando className "missions" em "Missions.js";
    - Criando div em "Missions.js";
    - Adicionando className "missions-infos" em "Missions.js".

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import Title from './Title';
 import missions from '../data/missions';
 import MissionCard from './MissionCard';
@@ -6,17 +7,22 @@ import MissionCard from './MissionCard';
 class Missions extends React.Component {
   render() {
     return (
-      <div data-testid="missions">
+      <div className="missions" data-testid="missions">
         <Title headline="Missões" />
-        {
-          missions.map((mission) => (<MissionCard
-            key={ mission.name }
-            name={ mission.name }
-            year={ mission.year }
-            country={ mission.country }
-            destination={ mission.destination }
-          />))
-        }
+
+        <div className="missions-infos">
+          {
+            missions.map((mission) => (
+              <MissionCard
+                key={ mission.name }
+                name={ mission.name }
+                year={ mission.year }
+                country={ mission.country }
+                destination={ mission.destination }
+              />
+            ))
+          }
+        </div>
       </div>
     );
   }
